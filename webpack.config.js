6de9e5a8340884a264e3edf9c38e30940f5c7380
J. N. Yupanqui P <jnyupanquip@gmail.com>
2023-11-06 08:59:12 -0600

    config babel

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,15 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "./dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/client/view/index.html",
